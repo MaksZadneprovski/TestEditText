6cde9830dff04c5d2eb1cd797db1785b9c0b8e15
MaksZadneprovski <maksysha40@gmail.com>
2022-11-30 00:02:59 +0300

    24

--- app/src/main/java/com/example/testedittext/activities/report_list/SettingsActivity.java
+++ app/src/main/java/com/example/testedittext/activities/report_list/SettingsActivity.java
@@ -1,12 +1,14 @@
 package com.example.testedittext.activities.report_list;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.testedittext.R;
 
@@ -15,29 +17,64 @@ public class SettingsActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
     public static final String APP_PREFERENCES = "mysettings";
     private SharedPreferences.Editor editor;
+    private ConstraintLayout clAuthor, clExit;
+    private String login, pass;
+    private  EditText loginET, passET;
+    TextView tvLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
-        EditText loginET = findViewById(R.id.login);
-        EditText passET = findViewById(R.id.pass);
+        loginET = findViewById(R.id.login);
+        passET = findViewById(R.id.pass);
         Button button = findViewById(R.id.button);
+        Button buttonExit = findViewById(R.id.buttonExit);
+        clAuthor = findViewById(R.id.constraintAuthorization);
+        clExit = findViewById(R.id.constraintExit);
+        tvLogin = findViewById(R.id.tvLogin);
+
 
         sharedPreferences = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
         editor = sharedPreferences.edit();
 
-        String login = sharedPreferences.getString("login", null);
-        String pass = sharedPreferences.getString("pass", null);
+        login = sharedPreferences.getString("login", null);
+        pass = sharedPreferences.getString("pass", null);
 
-        loginET.setText(login);
-        passET.setText(pass);
+        setVisibility();
 
         button.setOnClickListener(view -> {
-            editor.putString( "login", loginET.getText().toString() );
-            editor.putString( "pass", passET.getText().toString() );
+            login = loginET.getText().toString();
+            pass = passET.getText().toString();
+            editor.putString( "login", login );
+            editor.putString( "pass", pass );
             editor.apply();
+            setVisibility();
         });
+
+        buttonExit.setOnClickListener(view -> {
+            editor.remove( "login" );
+            editor.remove( "pass" );
+            editor.apply();
+            login = null;
+            pass = null;
+            loginET.setText("");
+            passET.setText("");
+            setVisibility();
+        });
+    }
+
+    private void   setVisibility(){
+        if (login == null && pass == null){
+            clExit.setVisibility(View.GONE);
+            clAuthor.setVisibility(View.VISIBLE);
+        }else {
+            login = sharedPreferences.getString("login", null);
+            pass = sharedPreferences.getString("pass", null);
+            clExit.setVisibility(View.VISIBLE);
+            clAuthor.setVisibility(View.GONE);
+            tvLogin.setText(login);
+        }
     }
 }
--- app/src/main/java/com/example/testedittext/report_creator/Report.java
+++ app/src/main/java/com/example/testedittext/report_creator/Report.java
@@ -36,6 +36,8 @@ public class Report {
     Context context;
     private  String fileName;
     ReportEntity report;
+    private static Font font8, font10;
+    CellStyle styleBorderNone;
 
     public Report(Context context, String fileName, ReportEntity report) {
         this.context = context;
@@ -45,26 +47,22 @@ public class Report {
 
 
 
-
     public void generate() throws IOException {
 
         Workbook wb;
-
-        wb = WorkbookFactory.create(context.getResources().openRawResource(R.raw.report2));
-
-        Sheet sheet = wb.getSheet("l2");
+        wb = WorkbookFactory.create(context.getResources().openRawResource(R.raw.report3));
 
         // Create a new font and alter it.
-        Font font = wb.createFont();
-        font.setFontHeightInPoints((short)8);
-        font.setFontName("Times New Roman");
-        font.setBold(false);
+        font8 = wb.createFont();
+        font8.setFontHeightInPoints((short)8);
+        font8.setFontName("Times New Roman");
+        font8.setBold(false);
 
         // Create a new font and alter it.
-        Font font2 = wb.createFont();
-        font2.setFontHeightInPoints((short)10);
-        font2.setFontName("Times New Roman");
-        font2.setBold(false);
+        font10 = wb.createFont();
+        font10.setFontHeightInPoints((short)10);
+        font10.setFontName("Times New Roman");
+        font10.setBold(false);
 
         // Создаем стиль для создания рамки у ячейки
         CellStyle style = wb.getCellStyleAt(1);
@@ -77,51 +75,39 @@ public class Report {
         style.setBorderTop(BorderStyle.THIN);
         style.setTopBorderColor(IndexedColors.BLACK.getIndex());
         style.setWrapText(true);
-        style.setFont(font);
+        style.setFont(font8);
         style.setAlignment(HorizontalAlignment.CENTER);
         style.setVerticalAlignment(VerticalAlignment.CENTER);
 
         ////////////////////////////////////////////////////////////////////////////////////////
 
-        // Создаем стиль для данных
-        CellStyle style2 = wb.getCellStyleAt(2);
-        style2.setBorderTop(BorderStyle.NONE);
-        style2.setBorderBottom(BorderStyle.NONE);
-        style2.setBorderLeft(BorderStyle.NONE);
-        style2.setBorderRight(BorderStyle.NONE);
-        style2.setWrapText(false);
-        style2.setFont(font);
-        style2.setAlignment(HorizontalAlignment.RIGHT);
-        style2.setVerticalAlignment(VerticalAlignment.CENTER);
+        // Создаем стиль для данных об объекте
+        styleBorderNone = wb.getCellStyleAt(2);
+        styleBorderNone.setBorderTop(BorderStyle.NONE);
+        styleBorderNone.setBorderBottom(BorderStyle.NONE);
+        styleBorderNone.setBorderLeft(BorderStyle.NONE);
+        styleBorderNone.setBorderRight(BorderStyle.NONE);
+        styleBorderNone.setWrapText(false);
+        styleBorderNone.setFont(font10);
+        styleBorderNone.setAlignment(HorizontalAlignment.RIGHT);
+        styleBorderNone.setVerticalAlignment(VerticalAlignment.CENTER);
 
 
-
-        ////////////////////////////////////////////////////////////////////////////////////////////
-
-        //
-        Row row1 = sheet.getRow(0);
-        Row row2 = sheet.getRow(1);
-        Row row3 = sheet.getRow(2);
-        Row row4 = sheet.getRow(3);
-
-        Cell cell_1_15 = row1.createCell(15);
-        Cell cell_2_15 = row2.createCell(15);
-        Cell cell_3_15 = row3.createCell(15);
-        Cell cell_4_15 = row4.createCell(15);
-
-        cell_1_15.setCellValue("Заказчик: " + report.getCustomer());
-        cell_2_15.setCellValue("Объект: " + report.getObject());
-        cell_3_15.setCellValue("Адрес: " + report.getAddress());
-        cell_4_15.setCellValue("Дата: " + report.getDate());
-
-
-        cell_1_15.setCellStyle(style2);
-        cell_2_15.setCellStyle(style2);
-        cell_3_15.setCellStyle(style2);
-        cell_4_15.setCellStyle(style2);
-////////////////////////////////////////////////////////////////////////////////////////////////////
         // Получаем щиты для составления отчета
         ArrayList<Shield> shields = report.getShields();
+
+// Созданиие протокола изоляции
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Sheet sheetInsulation = wb.getSheet("Insulation");
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // Заполняем строки заказчик, объект, адрес, дата
+        fillMainData(sheetInsulation, 15);
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Заполняем строку погоды
+        fillWeather(sheetInsulation, 13, 4);
+
         // Начинаем с 29 строки, первые 28 занимает шапка таблицы
         int countRow = 27;
         int paragraph = 1;
@@ -132,13 +118,11 @@ public class Report {
             // Проход по щитам
             for (int i = 0; i < shields.size(); i++) {
                 Shield shield = shields.get(i);
-                row = sheet.createRow(countRow);
+                row = sheetInsulation.createRow(countRow);
 
                 Cell cell;
 
-
-
-                sheet.addMergedRegion(new CellRangeAddress(
+                sheetInsulation.addMergedRegion(new CellRangeAddress(
                         countRow, //first row (0-based)
                         countRow, //last row  (0-based)
                         0, //first column (0-based)
@@ -165,10 +149,10 @@ public class Report {
 
                         if (!group.getAddress().isEmpty()) {
 
-                            row = sheet.createRow(countRow);
+                            row = sheetInsulation.createRow(countRow);
 
                             //увеличиваем высоту строки, чтобы вместить две строки текста
-                            row.setHeightInPoints((2*sheet.getDefaultRowHeightInPoints()));
+                            row.setHeightInPoints((2*sheetInsulation.getDefaultRowHeightInPoints()));
 
                             // Столбец пункт
                             cell = row.createCell(0);
@@ -210,7 +194,6 @@ public class Report {
                                 cell.setCellStyle(style);
                             }
 
-
                             // Нужно ли заполнять N-PE
                             boolean isPE = group.getNumberOfWireCores().equals("3") || group.getNumberOfWireCores().equals("5");
                             // Если в изоляции везде прочерки, "соотв." не заполнится
@@ -235,12 +218,8 @@ public class Report {
                                     conformity = true;
                                     break;
                             }
-
                             // Столбец соотв
                             if (conformity) row.getCell(15).setCellValue("соотв.");
-
-
-
                             countRow++;
                         }
                     }
@@ -249,10 +228,25 @@ public class Report {
         }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+        // Созданиие протокола петли
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Sheet sheetF0 = wb.getSheet("F0");
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        // Заполняем строки заказчик, объект, адрес, дата
+        fillMainData(sheetF0, 14);
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Заполняем строку погоды
+        fillWeather(sheetF0, 10, 6);
+
+
+// Создание файла
+////////////////////////////////////////////////////////////////////////////////////////////////////
         try (FileOutputStream fileOut = new FileOutputStream(getExternalPath())) {
             wb.write(fileOut);
         }
-
         wb.close();
     }
 
@@ -276,9 +270,45 @@ public class Report {
             row.getCell(14).setCellFormula(ExcelFormula.random);
         }
     }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     private File getExternalPath() {
         return new File(context.getExternalFilesDir(null), fileName);
     }
 
+    private void fillMainData(Sheet sheet, int column){
+        Row row1 = sheet.getRow(0);
+        Row row2 = sheet.getRow(1);
+        Row row3 = sheet.getRow(2);
+        Row row4 = sheet.getRow(3);
+
+        Cell cell_1_15 = row1.createCell(column);
+        Cell cell_2_15 = row2.createCell(column);
+        Cell cell_3_15 = row3.createCell(column);
+        Cell cell_4_15 = row4.createCell(column);
+
+        cell_1_15.setCellValue("Заказчик: " + report.getCustomer());
+        cell_2_15.setCellValue("Объект: " + report.getObject());
+        cell_3_15.setCellValue("Адрес: " + report.getAddress());
+        cell_4_15.setCellValue("Дата: " + report.getDate());
+
+        cell_1_15.setCellStyle(styleBorderNone);
+        cell_2_15.setCellStyle(styleBorderNone);
+        cell_3_15.setCellStyle(styleBorderNone);
+        cell_4_15.setCellStyle(styleBorderNone);
+    }
+
+    private void fillWeather(Sheet sheet,int row, int column){
+        Row r = sheet.getRow(row);
+        Cell c = r.createCell(column);
+        styleBorderNone.setAlignment(HorizontalAlignment.CENTER);
+        String data = "Температура воздуха " +
+                report.getTemperature() +
+                " °С.  Влажность воздуха " +
+                report.getHumidity() +
+                "%.  Атмосферное давление " +
+                report.getPressure() +
+                "  мм.рт.ст.";
+        c.setCellValue(data);
+        c.setCellStyle(styleBorderNone);
+    }
 }
